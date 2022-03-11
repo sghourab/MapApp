f6c989ca81d0e8493f81ed0554f439f029535e36
Samar Ghourag <samarghourab@gmail.com>
2022-03-11 13:16:16 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Summer Crow on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
